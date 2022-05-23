842d7c65d628edafbbf5f5efec99e31f8df2aa7c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-23 20:44:06 +0200

    Position the Logo and the Background using code - old fashion- way)

--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -2,9 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2  - swoosh.frame.size.width / 2,
+            y: 50,
+            width: swoosh.frame.size.width,
+            height: swoosh.frame.size.height)
+        backgroundImage.frame = view.frame
     }
 
 

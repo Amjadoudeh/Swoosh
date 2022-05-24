2f8f1a9691f1d464487ac4d444dd75518bc30a42
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-24 09:36:57 +0200

    Add cornerRadius to the Button border

--- Swoosh/Views/BorderButton.swift
+++ Swoosh/Views/BorderButton.swift
@@ -5,6 +5,7 @@ class BorderButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.borderWidth = 2.0
+        layer.cornerRadius = 10
         layer.borderColor =
             UIColor.white.cgColor
     }

c8e9608bc0f3a4eec23d9c1da29b5de9c1656ac3
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-27 12:44:10 +0200

    Create 3 bottons' actions

--- Swoosh/Controller/LeagueViewController.swift
+++ Swoosh/Controller/LeagueViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    @IBOutlet weak var nextButton: BorderButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,5 +10,17 @@ class LeagueViewController: UIViewController {
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    @IBAction func onMenTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onWomenTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        
     }
 }

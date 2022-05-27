5412cac4cdc51bc8c132b6712e10c7192777bf1c
Amjadoudeh <Amjad.oudeh88@gmail.com>
2022-05-27 12:23:28 +0200

    Perform Segue by passing identufier

--- Swoosh/Controller/LeagueViewController.swift
+++ Swoosh/Controller/LeagueViewController.swift
@@ -2,21 +2,12 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
-    */
-
 }

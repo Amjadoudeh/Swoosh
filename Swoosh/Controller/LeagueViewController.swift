827e8bb0ccf370e6feff0338efbda015ed2142cd
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2022-05-27 11:48:06 +0100

    selection style added

--- Swoosh/Controller/LeagueViewController.swift
+++ Swoosh/Controller/LeagueViewController.swift
@@ -1,10 +1,58 @@
 import UIKit
 
-class LeagueViewController: UIViewController {
+enum desiredLeague {
+    case men, woman, coed
+}
 
+class LeagueViewController: UIViewController {
+    // MARK: - Properties
+    private var selectedLeague: desiredLeague?
+    
+    @IBOutlet weak var menButton: BorderButton!
+    @IBOutlet weak var womanButton: BorderButton!
+    @IBOutlet weak var coedButton: BorderButton!
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    // MARK: - Set up
+    
+    
+    // MARK: - Functions
+    
+    private func selectedButton(selected: desiredLeague) {
+        switch selected {
+        case .men:
+            menButton.backgroundColor = UIColor.ButtonsColor.selectedInDesiredLeague
+            womanButton.backgroundColor = UIColor.ButtonsColor.notSelected
+            coedButton.backgroundColor = UIColor.ButtonsColor.notSelected
+        case .woman:
+            menButton.backgroundColor = UIColor.ButtonsColor.notSelected
+            womanButton.backgroundColor = UIColor.ButtonsColor.selectedInDesiredLeague
+            coedButton.backgroundColor = UIColor.ButtonsColor.notSelected
+        case .coed:
+            menButton.backgroundColor = UIColor.ButtonsColor.notSelected
+            womanButton.backgroundColor = UIColor.ButtonsColor.notSelected
+            coedButton.backgroundColor = UIColor.ButtonsColor.selectedInDesiredLeague
+        }
+    }
+    
+    
+    // MARK: - Actions
+    @IBAction func didSelectMan(_ sender: Any) {
+        selectedLeague = .men
+        selectedButton(selected: selectedLeague!)
+    }
+    @IBAction func didSelectWoman(_ sender: Any) {
+        selectedLeague = .woman
+        selectedButton(selected: selectedLeague!)
+    }
+    @IBAction func didSelectCoed(_ sender: Any) {
+        selectedLeague = .coed
+        selectedButton(selected: selectedLeague!)
+    }
+    
 
 }
+// MARK: - Extensions
